27f20943a59731a74b660b3400e94f8978509644
mukulsharmao7 <91932734+mukulsharmao7@users.noreply.github.com>
2026-04-23 18:55:46 +0530

    Add twoSum function in day15_two_sum.cpp
    Implement twoSum function to find indices of two numbers that add up to target.

---
+++ day15_two_sum.cpp
@@ -0,0 +1,21 @@
+// Time: O(n)
+ // Space: O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // value -> index
+        
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if(mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+            
+            mp[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
