9e34805511db222b7826853b0201dc83622d7a62
mukulsharmao7 <91932734+mukulsharmao7@users.noreply.github.com>
2026-05-05 21:16:14 +0530

    Add longestConsecutive function in Solution class
    Implement a solution to find the longest consecutive sequence in an array.

---
+++ day-26_longest_consecutive_sequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        int longest = 0;
+
+        for(int num : st) {
+            // start of sequence
+            if(st.find(num - 1) == st.end()) {
+                int current = num;
+                int count = 1;
+
+                while(st.find(current + 1) != st.end()) {
+                    current++;
+                    count++;
+                }
+
+                longest = max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+};
